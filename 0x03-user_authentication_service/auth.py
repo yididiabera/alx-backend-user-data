2063f547a34bfbfa7c94c729d2771ad1d4a1b92d
Jedidiah Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-22 23:50:42 +0300

    fix documentation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Authentication module"""
+"""Authentication Module"""
 import bcrypt
 import uuid
 from db import DB
@@ -8,24 +8,24 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """Hash a password"""
+    """Hash a password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid() -> str:
-    """Generates a new uuid"""
+    """Generate a new uuid"""
     return str(uuid.uuid4())
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
+    """Auth class to interact with the authentication Database.
     """
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Registers a user and returns the user object"""
+        """Register a user and returns the user object"""
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
@@ -42,7 +42,7 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """Returns session ID"""
+        """Return session ID"""
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
@@ -52,7 +52,7 @@ class Auth:
             return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
-        """Returns a logged in user"""
+        """Return a logged in user"""
         if session_id is None:
             return None
         try:
@@ -62,11 +62,11 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
-        """Destroys a session"""
+        """Destroy a session"""
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """Generates a reset password token"""
+        """Generate a reset password token"""
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
